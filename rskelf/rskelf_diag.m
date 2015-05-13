ca27de7142a708286261af2f3451fbdc4b599a7f
Kenneth L. Ho <klho@stanford.edu>
2015-05-13 00:05:48 -0700

    General clean-up and added diagonal extraction for rskelf.

---
+++ rskelf/rskelf_diag.m
@@ -0,0 +1,228 @@
+% RSKELF_DIAG   Extract diagonal using recursive skeletonization factorization.
+%
+%    D = RSKELF_DIAG(F) produces the diagonal D of the factored matrix F.
+%
+%    D = RSKELF_DIAG(F,DINV) computes the diagonal of F if DINV = 0 (default)
+%    and the diagonal of INV(F) if DINV = 1.
+%
+%    D = RSKEL_DIAGINV(F,DINV,OPTS) also passes various options to
+%    the algorithm. Valid options include:
+%
+%      - VERB: display status of the code if VERB = 1 (default: VERB = 0).
+%
+%    See also RSKELF.
+
+function D = rskelf_diag(F,dinv,opts)
+  start = tic;
+
+  % set default parameters
+  if nargin < 2
+    dinv = 0;
+  end
+  if nargin < 3
+    opts = [];
+  end
+  if ~isfield(opts,'verb')
+    opts.verb = 0;
+  end
+
+  % initialize
+  N = F.N;
+  nlvl = F.nlvl;
+  rem = true(N,1);
+  mnz = 128;
+  I = zeros(mnz,1);
+  J = zeros(mnz,1);
+  S = zeros(mnz,1);
+  P = zeros(N,1);
+
+  % get required entries at each level
+  tic
+  req = cell(nlvl,1);
+  req{1} = sparse(1:N,1:N,true(N,1),N,N);
+  for lvl = 1:nlvl-1
+    nz = 0;
+
+    % eliminate redundant DOFs
+    rem([F.factors(F.lvp(lvl)+1:F.lvp(lvl+1)).rd]) = 0;
+
+    % store previously computed entries
+    [I_,J_] = find(req{lvl});
+    idx = rem(I_) & rem(J_);
+    I_ = I_(idx);
+    J_ = J_(idx);
+    m = numel(I_);
+    while mnz < nz + m
+      e = zeros(mnz,1);
+      I = [I; e];
+      J = [J; e];
+      S = [S; e];
+      mnz = 2*mnz;
+    end
+    I(nz+1:nz+m) = I_;
+    J(nz+1:nz+m) = J_;
+    nz = nz + m;
+
+    % loop over nodes at current level
+    for i = F.lvp(lvl)+1:F.lvp(lvl+1)
+
+      % require skeleton entries
+      sk = F.factors(i).sk;
+      [I_,J_] = ndgrid(sk);
+      m = numel(I_);
+      while mnz < nz + m
+        e = zeros(mnz,1);
+        I = [I; e];
+        J = [J; e];
+        S = [S; e];
+        mnz = 2*mnz;
+      end
+      I(nz+1:nz+m) = I_;
+      J(nz+1:nz+m) = J_;
+      nz = nz + m;
+    end
+
+    % construct requirement matrix
+    idx = 1:nz;
+    if strcmpi(F.symm,'s') || strcmpi(F.symm,'h') || strcmpi(F.symm,'p')
+      idx = find(I(idx) >= J(idx));
+    end
+    req{lvl+1} = logical(sparse(I(idx),J(idx),ones(size(idx)),N,N));
+  end
+
+  % print summary
+  if opts.verb
+    req_ = req{1};
+    for lvl = 1:nlvl-1
+      req_ = req_ | req{lvl+1};
+    end
+    fprintf(['-'*ones(1,80) '\n'])
+    fprintf('%3s | %12d | %25.2e (s)\n','-',nnz(req_),toc)
+  end
+
+  % unfold factorization
+  M = sparse(N,N);
+  for lvl = nlvl:-1:1
+    tic
+    [I_,J_,S_] = find(M);
+    nz = length(S_);
+    I(1:nz) = I_;
+    J(1:nz) = J_;
+    S(1:nz) = S_;
+
+    % loop over nodes
+    for i = F.lvp(lvl)+1:F.lvp(lvl+1)
+      sk = F.factors(i).sk;
+      rd = F.factors(i).rd;
+      T = F.factors(i).T;
+      L = F.factors(i).L;
+      E = F.factors(i).E;
+      if strcmpi(F.symm,'n') || strcmpi(F.symm,'s')
+        G = F.factors(i).F;
+        U = F.factors(i).U;
+      elseif strcmpi(F.symm,'h') || strcmpi(F.symm,'p')
+        G = F.factors(i).E';
+        U = F.factors(i).L';
+      end
+
+      % compute local matrix
+      nrd = length(rd);
+      nsk = length(sk);
+      ird = 1:nrd;
+      isk = nrd+1:nrd+nsk;
+      D = zeros(nrd+nsk);
+      if strcmpi(F.symm,'h')
+        if dinv
+          D(ird,ird) = inv(F.factors(i).U);
+        else
+          D(ird,ird) = F.factors(i).U;
+        end
+      else
+        D(ird,ird) = eye(nrd);
+      end
+      Dsk = spget_sk;
+      D(isk,isk) = Dsk;
+      if dinv
+        D(:,ird) = (D(:,ird) - D(:,isk)*E)/L;
+        D(ird,:) = U\(D(ird,:) - G*D(isk,:));
+      else
+        D(:,isk) = D(:,isk) + D(:,ird)*G;
+        D(isk,:) = D(isk,:) + E*D(ird,:);
+      end
+      if dinv
+        if strcmp(F.symm,'s')
+          D(:,isk) = D(:,isk) - D(:,ird)*T.';
+        else
+          D(:,isk) = D(:,isk) - D(:,ird)*T';
+        end
+        D(isk,:) = D(isk,:) - T*D(ird,:);
+      else
+        D(:,ird) = D(:,ird)*U + D(:,isk)*T;
+        if strcmp(F.symm,'s')
+          D(ird,:) = L*D(ird,:) + T.'*D(isk,:);
+        else
+          D(ird,:) = L*D(ird,:) + T'*D(isk,:);
+        end
+      end
+      D(isk,isk) = D(isk,isk) - Dsk;
+
+      % store update to global sparse matrix
+      [I_,J_] = ndgrid([rd sk]);
+      S_ = D(:);
+      m = length(S_);
+      while mnz < nz + m
+        e = zeros(mnz,1);
+        I = [I; e];
+        J = [J; e];
+        S = [S; e];
+        mnz = 2*mnz;
+      end
+      I(nz+1:nz+m) = I_;
+      J(nz+1:nz+m) = J_;
+      S(nz+1:nz+m) = S_;
+      nz = nz + m;
+    end
+
+    % keep only remaining entries
+    [I_,J_] = find(req{lvl});
+    idx = ismembc(N*I(1:nz)+J(1:nz),sort(N*I_+J_));
+
+    % update global sparse matrix
+    M = sparse(I(idx),J(idx),S(idx),N,N);
+
+    % print summary
+    if opts.verb
+      fprintf('%3d | %12d | %12d | %10.2e (s)\n',lvl,nnz(idx),nnz(M),toc)
+    end
+  end
+
+  % finish
+  D = spdiags(M,0);
+  if opts.verb
+    fprintf(['-'*ones(1,80) '\n'])
+    toc(start)
+  end
+
+  % sparse matrix access function (native MATLAB is slow for large matrices)
+  function A = spget_sk
+    A = zeros(nsk);
+    [I_,J_,S_] = find(M(:,sk));
+    idx = ismembc(I_,sort(sk));
+    I_ = I_(idx);
+    J_ = J_(idx);
+    S_ = S_(idx);
+    P(sk) = 1:nsk;
+    idx = P(I_) + (J_ - 1)*nsk;
+    A(idx) = A(idx) + S_;
+    if ~strcmpi(F.symm,'n')
+      D_ = diag(diag(A));
+      L_ = tril(A,-1);
+      U_ = triu(A, 1);
+      if strcmpi(F.symm,'s')
+        A = D_ + L_ + L_.' + U_ + U_.';
+      elseif strcmpi(F.symm,'h') || strcmpi(F.symm,'p')
+        A = D_ + L_ + L_' + U_ + U_';
+      end
+    end
+  end
+end
---
+++ rskelf/test/cov_circle1.m
@@ -0,0 +1,150 @@
+% Squared exponential covariance function on the unit circle.
+
+function cov_circle1(n,occ,p,rank_or_tol,noise,scale)
+
+  % set default parameters
+  if nargin < 1 || isempty(n)
+    n = 16384;
+  end
+  if nargin < 2 || isempty(occ)
+    occ = 64;
+  end
+  if nargin < 3 || isempty(p)
+    p = 16;
+  end
+  if nargin < 4 || isempty(rank_or_tol)
+    rank_or_tol = 1e-12;
+  end
+  if nargin < 5 || isempty(noise)
+    noise = 1e-2;
+  end
+  if nargin < 6 || isempty(scale)
+    scale = 100;
+  end
+
+  % initialize
+  theta = (1:n)*2*pi/n;
+  x = [cos(theta); sin(theta)];
+  N = size(x,2);
+  theta = (1:p)*2*pi/p;
+  proxy_ = [cos(theta); sin(theta)];
+  proxy = [];
+  for r = linspace(1.5,2.5,p)
+    proxy = [proxy r*proxy_];
+  end
+
+  % factor matrix
+  opts = struct('symm','p','verb',1);
+  F = rskelf(@Afun,x,occ,rank_or_tol,@pxyfun,opts);
+  w = whos('F');
+  fprintf([repmat('-',1,80) '\n'])
+  fprintf('mem: %6.2f (MB)\n',w.bytes/1e6)
+
+  % set up FFT multiplication
+  G = fft(Afun(1:N,1));
+
+  % test accuracy using randomized power method
+  X = rand(N,1);
+  X = X/norm(X);
+
+  % NORM(A - F)/NORM(A)
+  tic
+  rskelf_mv(F,X);
+  t = toc;
+  [e,niter] = snorm(N,@(x)(mv(x) - rskelf_mv(F,x)),[],[],1);
+  e = e/snorm(N,@mv,[],[],1);
+  fprintf('mv: %10.4e / %4d / %10.4e (s)\n',e,niter,t)
+
+  % NORM(INV(A) - INV(F))/NORM(INV(A)) <= NORM(I - A*INV(F))
+  tic
+  rskelf_sv(F,X);
+  t = toc;
+  [e,niter] = snorm(N,@(x)(x - mv(rskelf_sv(F,x))),[],[],1);
+  fprintf('sv: %10.4e / %4d / %10.4e (s)\n',e,niter,t)
+
+  % NORM(F - C*C')/NORM(F)
+  tic
+  rskelf_cholmv(F,X);
+  t = toc;
+  [e,niter] = snorm(N,@(x)(rskelf_mv(F,x) ...
+                         - rskelf_cholmv(F,rskelf_cholmv(F,x,'c'))),[],[],1);
+  e = e/snorm(N,@(x)(rskelf_mv(F,x)),[],[],1);
+  fprintf('cholmv: %10.4e / %4d / %10.4e (s)\n',e,niter,t)
+
+  % NORM(INV(F) - INV(C')*INV(C))/NORM(INV(F))
+  tic
+  rskelf_cholsv(F,X);
+  t = toc;
+  [e,niter] = snorm(N,@(x)(rskelf_sv(F,x) ...
+                         - rskelf_cholsv(F,rskelf_cholsv(F,x),'c')),[],[],1);
+  e = e/snorm(N,@(x)(rskelf_sv(F,x)),[],[],1);
+  fprintf('cholsv: %10.4e / %4d / %10.4e (s)\n',e,niter,t)
+
+  % compute log-determinant
+  tic
+  ld = rskelf_logdet(F);
+  t = toc;
+  fprintf('logdet: %22.16e / %10.4e (s)\n',ld,t)
+
+  % prepare for diagonal extracation
+  opts = struct('verb',1);
+  r = randperm(N);
+  r = r(1:min(N,16));
+  m = length(r);
+  X = zeros(N,m);
+  for i = 1:m
+    X(r(i),i) = 1;
+  end
+  E = zeros(m,1);
+
+  % extract diagonal
+  D = rskelf_diag(F,0,opts);
+  Y = rskelf_mv(F,X);
+  for i = 1:m
+    E(i) = Y(r(i),i);
+  end
+  e1 = norm(D(r) - E)/norm(E);
+
+  % extract diagonal of inverse
+  D = rskelf_diag(F,1,opts);
+  Y = rskelf_sv(F,X);
+  for i = 1:m
+    E(i) = Y(r(i),i);
+  end
+  e2 = norm(D(r) - E)/norm(E);
+
+  % print summary
+  fprintf([repmat('-',1,80) '\n'])
+  fprintf('diag: %10.4e / %10.4e\n',e1,e2)
+
+  % kernel function
+  function K = Kfun(x,y)
+    dx = bsxfun(@minus,x(1,:)',y(1,:));
+    dy = bsxfun(@minus,x(2,:)',y(2,:));
+    dr = scale*sqrt(dx.^2 + dy.^2);
+    K = exp(-0.5*dr.^2);
+  end
+
+  % matrix entries
+  function A = Afun(i,j)
+    A = Kfun(x(:,i),x(:,j));
+    [I,J] = ndgrid(i,j);
+    idx = I == J;
+    A(idx) = A(idx) + noise^2;
+  end
+
+  % proxy function
+  function [Kpxy,nbr] = pxyfun(x,slf,nbr,l,ctr)
+    pxy = bsxfun(@plus,proxy*l,ctr');
+    Kpxy = Kfun(pxy,x(:,slf));
+    dx = x(1,nbr) - ctr(1);
+    dy = x(2,nbr) - ctr(2);
+    dist = sqrt(dx.^2 + dy.^2);
+    nbr = nbr(dist/l < 1.5);
+  end
+
+  % FFT multiplication
+  function y = mv(x)
+    y = ifft(G.*fft(x));
+  end
+end
