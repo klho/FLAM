582929d10dbc6155ad3fccfefad7b02ac939f01d
Kenneth L. Ho <klho@stanford.edu>
2015-05-16 13:15:16 -0700

    Moved ie_ellipse example to rskelf, added cov examples to rskel.

---
+++ rskel/test/cov_line1.m
@@ -0,0 +1,210 @@
+% Squared exponential covariance function on the unit line.
+
+function cov_line1(n,occ,p,rank_or_tol,symm,noise,scale)
+
+  % set default parameters
+  if nargin < 1 || isempty(n)
+    n = 16384;
+  end
+  if nargin < 2 || isempty(occ)
+    occ = 64;
+  end
+  if nargin < 3 || isempty(p)
+    p = 8;
+  end
+  if nargin < 4 || isempty(rank_or_tol)
+    rank_or_tol = 1e-12;
+  end
+  if nargin < 5 || isempty(symm)
+    symm = 'p';
+  end
+  if nargin < 6 || isempty(noise)
+    noise = 1e-2;
+  end
+  if nargin < 7 || isempty(scale)
+    scale = 100;
+  end
+
+  % initialize
+  x = (1:n)/n;
+  N = size(x,2);
+  proxy = linspace(1.5,2.5,p);
+  proxy = [-proxy proxy];
+
+  % compress matrix
+  opts = struct('symm',symm,'verb',1);
+  F = rskel(@Afun,x,x,occ,rank_or_tol,@pxyfun,opts);
+  w = whos('F');
+  fprintf([repmat('-',1,80) '\n'])
+  fprintf('mem: %6.2f (MB)\n',w.bytes/1e6)
+
+  % factor extended sparsification
+  tic
+  A = rskel_xsp(F);
+  t = toc;
+  w = whos('A');
+  fprintf('xsp: %10.4e (s) / %6.2f (MB)\n',t,w.bytes/1e6);
+  tic
+  if strcmpi(F.symm,'n')
+    [L,U] = lu(A);
+  else
+    [L,D,P] = ldl(A);
+  end
+  t = toc;
+  if strcmpi(F.symm,'n')
+    w = whos('L');
+    spmem = w.bytes;
+    w = whos('U');
+    spmem = (spmem + w.bytes)/1e6;
+  else
+    w = whos('L');
+    spmem = w.bytes;
+    w = whos('D');
+    spmem = (spmem + w.bytes)/1e6;
+  end
+  fprintf('lu/ldl: %10.4e (s) / %6.2f (MB)\n',t,spmem)
+
+  % set up FFT multiplication
+  a = Afun(1:n,1);
+  B = zeros(2*n-1,1);
+  B(1:n) = a;
+  B(n+1:end) = flipud(a(2:n));
+  G = fft(B);
+
+  % test accuracy using randomized power method
+  X = rand(N,1);
+  X = X/norm(X);
+
+  % NORM(A - F)/NORM(A)
+  tic
+  rskel_mv(F,X);
+  t = toc;
+  [e,niter] = snorm(N,@(x)(mv(x) - rskel_mv(F,x)),[],[],1);
+  e = e/snorm(N,@mv,[],[],1);
+  fprintf('mv: %10.4e / %4d / %10.4e (s)\n',e,niter,t)
+
+  % NORM(INV(A) - INV(F))/NORM(INV(A)) <= NORM(I - A*INV(F))
+  tic
+  sv(X);
+  t = toc;
+  [e,niter] = snorm(N,@(x)(x - mv(sv(x))),[],[],1);
+  fprintf('sv: %10.4e / %4d / %10.4e (s)\n',e,niter,t)
+
+  % prepare for selected inversion
+  m = 16;
+  r = randi(N,m,2);
+  X = zeros(N,m);
+  for i = 1:m
+    X(r(i,2),i) = 1;
+  end
+  ei = sparse([],[],[],size(A,1),1,1);
+  ej = sparse([],[],[],size(A,1),1,1);
+  S = zeros(m,1);
+  T = zeros(m,1);
+  tic
+  if strcmp(F.symm,'n')
+    Li  = inv(L);
+    Uic = inv(U)';
+    nz = 0.5*(nnz(Li(:,1:N)) + nnz(Uic(:,1:N)))/N;
+    w = whos('Li');
+    spmem = w.bytes;
+    w = whos('Uic');
+    spmem = (spmem + w.bytes)/1e6;
+  else
+    Li = inv(L);
+    Di = inv(D);
+    Pi = inv(P);
+    nz = nnz(Li(:,1:N))/N;
+    w = whos('Li');
+    spmem = w.bytes;
+    w = whos('Di');
+    spmem = spmem + w.bytes;
+    w = whos('Pi');
+    spmem = (spmem + w.bytes)/1e6;
+  end
+  t = toc;
+  fprintf('inv: %10.4e (s) / %10.4e / %6.2f (MB)\n',t,nz,spmem)
+
+  % selected inversion
+  tic
+  for i = 1:m
+    if strcmp(F.symm,'n')
+      S(i) = dot(Uic(:,r(i,1)),Li(:,r(i,2)));
+    else
+      ei(r(i,1)) = 1;
+      ej(r(i,2)) = 1;
+      S(i) = dot(Li*(Pi*ei),Di*(Li*(Pi*ej)));
+      ei(r(i,1)) = 0;
+      ej(r(i,2)) = 0;
+    end
+  end
+  t = toc/m;
+  Y = sv(X);
+  for i = 1:m
+    T(i) = Y(r(i,1),i);
+  end
+  e = norm(S - T)/norm(T);
+  fprintf('selinv: %10.4e / %10.4e (s)\n',e,t)
+
+  % diagonal inversion
+  tic
+  for i = 1:m
+    if strcmp(F.symm,'n')
+      S(i) = dot(Uic(:,r(i,2)),Li(:,r(i,2)));
+    else
+      ei(r(i,2)) = 1;
+      ej(r(i,2)) = 1;
+      S(i) = dot(Li*(Pi*ei),Di*(Li*(Pi*ej)));
+      ei(r(i,2)) = 0;
+      ej(r(i,2)) = 0;
+    end
+  end
+  t = toc/m;
+  Y = sv(X);
+  for i = 1:m
+    T(i) = Y(r(i,2),i);
+  end
+  e = norm(S - T)/norm(T);
+  fprintf('diaginv: %10.4e / %10.4e (s)\n',e,t)
+
+  % kernel function
+  function K = Kfun(x,y)
+    dr = scale*abs(bsxfun(@minus,x',y));
+    K = exp(-0.5*dr.^2);
+  end
+
+  % matrix entries
+  function A = Afun(i,j)
+    A = Kfun(x(:,i),x(:,j));
+    [I,J] = ndgrid(i,j);
+    idx = I == J;
+    A(idx) = A(idx) + noise^2;
+  end
+
+  % proxy function
+  function [Kpxy,nbr] = pxyfun(rc,rx,cx,slf,nbr,l,ctr)
+    pxy = bsxfun(@plus,proxy*l,ctr');
+    if strcmpi(rc,'r')
+      Kpxy = Kfun(rx(:,slf),pxy);
+    elseif strcmpi(rc,'c')
+      Kpxy = Kfun(pxy,cx(:,slf));
+    end
+  end
+
+  % FFT multiplication
+  function y = mv(x)
+    y = ifft(G.*fft(x,2*n-1));
+    y = y(1:n);
+  end
+
+  % sparse LU solve
+  function Y = sv(X)
+    X = [X; zeros(size(A,1)-N,size(X,2))];
+    if strcmpi(F.symm,'n')
+      Y = U\(L\X);
+    else
+      Y = P*(L'\(D\(L\(P'*X))));
+    end
+    Y = Y(1:N,:);
+  end
+end
--- rskelf/test/ie_ellipse.m
+++ rskelf/test/ie_ellipse.m
@@ -1,13 +1,13 @@
 % Second-kind integral equation on an ellipse, Laplace double-layer.
 
-function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
+function ie_ellipse(n,occ,p,rank_or_tol,ratio)
 
   % set default parameters
   if nargin < 1 || isempty(n)
     n = 16384;
   end
   if nargin < 2 || isempty(occ)
-    occ = 128;
+    occ = 64;
   end
   if nargin < 3 || isempty(p)
     p = 64;
@@ -31,39 +31,24 @@ function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
   theta = (1:p)*2*pi/p;
   proxy = 1.5*[cos(theta); sin(theta)];
 
-  % compress matrix
+  % factor matrix
   opts = struct('verb',1);
-  F = rskel(@Afun,x,x,occ,rank_or_tol,@pxyfun,opts);
+  F = rskelf(@Afun,x,occ,rank_or_tol,@pxyfun,opts);
   w = whos('F');
   fprintf([repmat('-',1,80) '\n'])
   fprintf('mem: %6.2f (MB)\n',w.bytes/1e6)
-
-  % factor extended sparsification
-  tic
-  A = rskel_xsp(F);
-  t = toc;
-  w = whos('A');
-  fprintf('xsp: %10.4e (s) / %6.2f (MB)\n',t,w.bytes/1e6);
-  tic
-  [L,U] = lu(A);
-  t = toc;
-  w = whos('L');
-  spmem = w.bytes;
-  w = whos('U');
-  spmem = (spmem + w.bytes)/1e6;
-  fprintf('lu: %10.4e (s) / %6.2f (MB)\n',t,spmem)
 
   % test matrix apply accuracy
   X = rand(N,1);
   X = X/norm(X);
   tic
-  rskel_mv(F,X,'n');
+  rskelf_mv(F,X,'n');
   t = toc;
   X = rand(N,16);
   X = X/norm(X);
   r = randperm(N);
   r = r(1:min(N,128));
-  Y = rskel_mv(F,X,'n');
+  Y = rskelf_mv(F,X,'n');
   Z = Afun(r,1:N)*X;
   e = norm(Z - Y(r,:))/norm(Z);
   fprintf('mv:  %10.4e / %10.4e (s)\n',e,t)
@@ -72,13 +57,13 @@ function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
   X = rand(N,1);
   X = X/norm(X);
   tic
-  rskel_mv(F,X,'c');
+  rskelf_mv(F,X,'c');
   t = toc;
   X = rand(N,16);
   X = X/norm(X);
   r = randperm(N);
   r = r(1:min(N,128));
-  Y = rskel_mv(F,X,'c');
+  Y = rskelf_mv(F,X,'c');
   Z = Afun(1:N,r)'*X;
   e = norm(Z - Y(r,:))/norm(Z);
   fprintf('mva: %10.4e / %10.4e (s)\n',e,t)
@@ -87,16 +72,31 @@ function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
   X = rand(N,1);
   X = X/norm(X);
   tic
-  sv(X);
+  rskelf_sv(F,X,'n');
   t = toc;
   X = rand(N,16);
   X = X/norm(X);
   r = randperm(N);
   r = r(1:min(N,128));
-  Y = sv(X);
+  Y = rskelf_sv(F,X,'n');
   Z = Afun(r,1:N)*Y;
   e = norm(X(r,:) - Z)/norm(X(r,:));
   fprintf('sv: %10.4e / %10.4e (s)\n',e,t)
+
+  % test matrix inverse adjoint apply accuracy
+  X = rand(N,1);
+  X = X/norm(X);
+  tic
+  rskelf_sv(F,X,'c');
+  t = toc;
+  X = rand(N,16);
+  X = X/norm(X);
+  r = randperm(N);
+  r = r(1:min(N,128));
+  Y = rskelf_sv(F,X,'c');
+  Z = Afun(1:N,r)'*Y;
+  e = norm(X(r,:) - Z)/norm(X(r,:));
+  fprintf('sva: %10.4e / %10.4e (s)\n',e,t)
 
   % generate field due to exterior sources
   m = 16;
@@ -106,7 +106,7 @@ function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
   B = Kfun(x,src,'s')*q;
 
   % solve for surface density
-  X = sv(B);
+  X = rskelf_sv(F,B);
 
   % evaluate field at interior targets
   trg = 0.5*[ratio*cos(theta); sin(theta)];
@@ -116,53 +116,6 @@ function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
   Z = Kfun(trg,src,'s')*q;
   e = norm(Z - Y)/norm(Z);
   fprintf('pde: %10.4e\n',e)
-
-  % prepare for selected inversion
-  m = 16;
-  r = randi(N,m,2);
-  X = zeros(N,m);
-  for i = 1:m
-    X(r(i,2),i) = 1;
-  end
-  S = zeros(m,1);
-  T = zeros(m,1);
-  tic
-  Li = inv(L);
-  Uic = inv(U)';
-  t = toc;
-  nzLi = nnz(Li(:,1:N))/N;
-  nzUi = nnz(Uic(:,1:N))/N;
-  w = whos('Li');
-  spmem = w.bytes;
-  w = whos('Uic');
-  spmem = (spmem + w.bytes)/1e6;
-  fprintf('inv: %10.4e / %10.4e / %10.4e (s) / %6.2f (MB)\n',nzLi,nzUi,t,spmem)
-
-  % selected inversion
-  tic
-  for i = 1:m
-    S(i) = dot(Uic(:,r(i,1)),Li(:,r(i,2)));
-  end
-  t = toc/m;
-  Y = sv(X);
-  for i = 1:m
-    T(i) = Y(r(i,1),i);
-  end
-  e = norm(S - T)/norm(T);
-  fprintf('selinv: %10.4e / %10.4e (s)\n',e,t)
-
-  % diagonal inversion
-  tic
-  for i = 1:m
-    S(i) = dot(Uic(:,r(i,2)),Li(:,r(i,2)));
-  end
-  t = toc/m;
-  Y = sv(X);
-  for i = 1:m
-    T(i) = Y(r(i,2),i);
-  end
-  e = norm(S - T)/norm(T);
-  fprintf('diaginv: %10.4e / %10.4e (s)\n',e,t)
 
   % kernel function
   function K = Kfun(x,y,lp,nu)
@@ -192,25 +145,12 @@ function selinv_ie_ellipse(n,occ,p,rank_or_tol,ratio)
   end
 
   % proxy function
-  function [Kpxy,nbr] = pxyfun(rc,rx,cx,slf,nbr,l,ctr)
+  function [Kpxy,nbr] = pxyfun(x,slf,nbr,l,ctr)
     pxy = bsxfun(@plus,proxy*l,ctr');
-    if strcmpi(rc,'r')
-      Kpxy = Kfun(rx(:,slf),pxy,'s')*(2*pi/N);
-      dx = cx(1,nbr) - ctr(1);
-      dy = cx(2,nbr) - ctr(2);
-    elseif strcmpi(rc,'c')
-      Kpxy = bsxfun(@times,Kfun(pxy,cx(:,slf),'d',nu(:,slf)),h(slf));
-      dx = rx(1,nbr) - ctr(1);
-      dy = rx(2,nbr) - ctr(2);
-    end
+    Kpxy = bsxfun(@times,Kfun(pxy,x(:,slf),'d',nu(:,slf)),h(slf));
+    dx = x(1,nbr) - ctr(1);
+    dy = x(2,nbr) - ctr(2);
     dist = sqrt(dx.^2 + dy.^2);
     nbr = nbr(dist/l < 1.5);
-  end
-
-  % sparse LU solve
-  function Y = sv(X)
-    X = [X; zeros(size(A,1)-N,size(X,2))];
-    Y = U\(L\X);
-    Y = Y(1:N,:);
   end
 end
