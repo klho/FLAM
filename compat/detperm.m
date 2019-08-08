d3455262df412195b6c8cef592c44d0e98c28cc0
Ken Ho <klho@alumni.caltech.edu>
2019-08-07 21:43:02 -0700

    Standardize spacing.

--- compat/detperm.m
+++ compat/detperm.m
@@ -1,14 +1,15 @@
 % DETPERM  Determinant of permutation vector.
 %
-%   D = DETPERM(P) computes the determinant (i.e., sign) D of the permutation P.
+%    D = DETPERM(P) computes the determinant (i.e., sign) D of the permutation
+%    P.
 %
-%   In both MATLAB and Octave, this is done by calling DET on the associated
-%   permutation matrix, which for efficiency should be represented explicitly as
-%   sparse in the former but as dense (internally recognized as a permutation
-%   matrix) in the latter.
+%    In both MATLAB and Octave, this is done by calling DET on the associated
+%    permutation matrix, which for efficiency should be represented explicitly
+%    as sparse in the former but as dense (internally recognized as a
+%    permutation matrix) in the latter.
 %
-%   The determinant can be computed in linear time, which both the MATLAB and
-%   Octave implementations seem to achieve.
+%    The determinant can be computed in linear time, which both the MATLAB and
+%    Octave implementations seem to achieve.
 
 function d = detperm(p)
   n = length(p);
