031e458fcdd7dbbdecc7c6bda83e4d623a68c985
Ken Ho <klho@alumni.caltech.edu>
2020-05-20 19:00:48 -0700

    Add RRQR postprocessing to ID.

--- core/id.m
+++ core/id.m
@@ -12,20 +12,40 @@
 %    [SK,RD,T] = ID(A,RANK_OR_TOL) sets K = RANK_OR_TOL if RANK_OR_TOL >= 1 and
 %    TOL = RANK_OR_TOL if RANK_OR_TOL < 1.
 %
-%    [SK,RD] = ID(A,...) skips the computation of the interpolation matrix T.
+%    [SK,RD,T] = ID(A,RANK_OR_TOL,TMAX) iteratively refines the approximation
+%    using rank-revealing QR column swaps until 1 <= MAX(ABS(T(:))) <= TMAX.
+%    Ignored if TMAX = INF (default: TMAX = 2). Refinement can improve numerical
+%    stability and reduce the output rank.
+%
+%    [SK,RD,T] = ID(A,RANK_OR_TOL,TMAX,RRQR_ITER) performs at most RRQR
+%    refinement iterations (default: RRQR_ITER = INF).
+%
+%    [SK,RD,T,NITER] = ID(A,RANK_OR_TOL,...) also returns the number NITER of
+%    RRQR iterations performed.
 %
 %    References:
 %
 %      H. Cheng, Z. Gimbutas, P.G. Martinsson, V. Rokhlin. On the compression of
-%        low rank matrices. SIAM J. Sci. Comput. 26(4): 1389-1404, 2005.
+%        low rank matrices. SIAM J. Sci. Comput. 26 (4): 1389-1404, 2005.
+%
+%      M. Gu, S.C. Eisenstat. Efficient algorithms for computing a strong rank-
+%        revealing QR factorization. SIAM J. Sci. Comput. 17 (4): 848-869, 1996.
 %
 %    See also QR.
 
-function [sk,rd,T] = id(A,rank_or_tol)
+function [sk,rd,T,niter] = id(A,rank_or_tol,Tmax,rrqr_iter)
+
+  % set default parameters
+  if nargin < 3 || isempty(Tmax), Tmax = 2; end
+  if nargin < 4 || isempty(rrqr_iter), rrqr_iter = Inf; end
 
   % check inputs
   assert(rank_or_tol >= 0,'FLAM:id:invalidRankOrTol', ...
          'Rank or tolerance must be nonnegative.')
+  assert(Tmax >= 1, 'FLAM:id:invalidTmax', ...
+         'Interpolation matrix entry bound must be >= 1.')
+  assert(rrqr_iter >= 0,'FLAM:id:invalidRRQRIter', ...
+         'Maximum number of RRQR iterations must be nonnegative.')
 
   % initialize
   [m,n] = size(A);
@@ -33,18 +53,147 @@ function [sk,rd,T] = id(A,rank_or_tol)
   % return if matrix is empty
   if isempty(A)
     sk = []; rd = 1:n;
-    if nargout > 2, T = zeros(0,n); end
+    T = zeros(0,n);
     return
   end
 
   % reduce row size if too rectangular
-  if m > 8*n, [Q,A] = qr(A,0); end
+  if m > 8*n, [~,A] = qr(A,0); end
 
   % compute ID
-  [~,R,P] = qr(A,0);
-  if rank_or_tol < 1, k = nnz(abs(diag(R)) > abs(R(1))*rank_or_tol);
-  else,               k = min(rank_or_tol,n);
+  [~,R,p] = qr(A,0);
+  if rank_or_tol < 1, tol = abs(R(1))*rank_or_tol;
+  else,               tol = 0;
   end
-  sk = P(1:k); rd = P(k+1:end);
-  if nargout > 2, T = R(1:k,1:k)\R(1:k,k+1:end); end
+  k = nnz(abs(diag(R)) > tol);
+  R = R(1:k,:);
+  if rank_or_tol >= 1, k = min(rank_or_tol,n); end
+  R(1:k,k+1:end) = R(1:k,1:k)\R(1:k,k+1:end);  % store T
+
+  % RRQR postprocessing
+  niter = 0;
+  if ~isinf(Tmax) && rrqr_iter > 0 && k < n
+    f2 = Tmax^2;                      % convergence criterion
+    c2 = sum(R(k+1:end,k+1:end).^2);  % column norms of residual part
+    r2 = sum(inv(R(1:k,1:k)).^2,2);   % inverse row norms of main part
+    conv  = 0;                        % converged?
+
+    % one-loop variant of Gu-Eisenstat -- allows early rank reduction
+    while niter < rrqr_iter
+      tmp = R(1:k,k+1:end).^2 + r2*c2;
+      [t2,idx] = max(abs(tmp(:)));
+      if t2 <= f2, conv = 1; break; end  % converged
+      niter = niter + 1;
+      [i,j] = ind2sub([k n-k],idx);  % need to swap i-th and (k+j)-th columns
+
+      % swap i-th and k-th columns
+      if i < k
+        p([i k]) = p([k i]);
+        [~,R(1:k,1:k)] = qrupdate(eye(k),R(1:k,1:k),R(1:k,k)-R(1:k,i), ...
+                                  ((1:k == i)-(1:k == k))');
+        R([i k],k+1:end) = R([k i],k+1:end);
+        r2([i k]) = r2([k i]);
+      end
+
+      % swap (k+1)-th and (k+j)-th columns
+      if j > 1
+        p(k+[1 j]) = p(k+[j 1]);
+        R(:,k+[1 j]) = R(:,k+[j 1]);
+        c2([1 j]) = c2([j 1]);
+      end
+
+      % downdate inverse row norms
+      r2(1:k-1) = r2(1:k-1) - abs(R(1:k-1,1:k-1)\R(1:k-1,k)/R(k,k)).^2;
+      r2(k) = 0;
+
+      % The following updates the R = [R11 R12; 0 R22] in two steps, recalling
+      % that R12 actually stores T = R11\R12.
+      %
+      %   - First do a "half-update" of T and an otherwise full update of R,
+      %     i.e., overwrite R11 and R22 with R11_new and R22_new, respectively,
+      %     and T with R11\R12_new.
+      %
+      %   - Then do a rank-one update of T to obtain R11_new\R12_new.
+      %
+      % Note that the k-th and (k+1)-th columns are stored in swapped order
+      % during the first step below.
+
+      % half-update T and full-update R
+      if size(R,1) == k  % no trailing R22 -- typical for fixed-precision case
+        u = R(:,k+1);    % for rank-one update of T
+        R(:,k+1) = R(:,1:k)*R(:,k+1);
+        R(1:k-1,k) = 0; R(k,k) = 1;
+      else
+        % zero out R22(2:end,:) by Householder transformation
+        v = R(k+1:end,k+1);
+        v(1) = v(1) + sqrt(c2(1))*exp(1i*arg(R(k+1,k+1)));
+        v = v/norm(v);
+        R(k+1:end,k+2:end) = R(k+1:end,k+2:end) - 2*v*(v'*R(k+1:end,k+2:end));
+        R(k+2:end,k+1) = 0; R(k+1,k+1) = sqrt(c2(1));
+
+        % restore certain entries of T to "R space"
+        R(1:k,k+1) = R(1:k,1:k)*R(1:k,k+1);
+        R(k,k+2:end) = R(k,k)*R(k,k+2:end);
+        r = R(1:k,k);
+        s = R(k,k+2:end);
+
+        % zero out R21 after swap by Givens rotation and update column norms
+        c2 = c2 - abs(R(k+1,k+1:end)).^2;
+        R(k:k+1,k:end) = givens(R(k,k+1),R(k+1,k+1))*R(k:k+1,k:end);
+        c2 = c2 + abs(R(k+1,k+1:end)).^2;
+        c2(1) = abs(R(k+1,k))^2;
+
+        tmp = R(k,k); R(k,k) = r(k);
+        u = R(1:k,1:k)\R(1:k,k+1);  % for rank-one update of T
+        R(k,k) = tmp;
+
+        % half-update T
+        v = R(1:k-1,1:k-1)\r(1:k-1);
+        R(k,k) = R(k,k)/r(k);
+        R(1:k-1,k) = v*(1 - R(k,k));
+        R(1:k-1,k+2:end) = R(1:k-1,k+2:end) + v/r(k)*(s - R(k,k+2:end));
+        R(k,k+2:end) = R(k,k+2:end)/r(k);
+      end
+
+      % swap k-th and (k+1)-th columns; update inverse row norms
+      p([k k+1]) = p([k+1 k]);
+      R(:,[k k+1]) = R(:,[k+1 k]);
+      r2(1:k-1) = r2(1:k-1) + abs(R(1:k-1,1:k-1)\R(1:k-1,k)/R(k,k)).^2;
+      r2(k) = 1/abs(R(k,k))^2;
+
+      % finish update of T by Sherman-Morrison
+      u(k) = u(k) - 1;
+      R(1:k,k+1:end) = R(1:k,k+1:end) - u*R(k,k+1:end)/(1 + u(k));
+
+      % check if tolerance can be met by reducing rank
+      [r,i] = min(1./sqrt(r2));
+      if r > tol, continue; end  % no -- loop again
+
+      % can drop i-th column from skeletons; first swap with k-th column
+      if i < k
+        p([i k]) = p([k i]);
+        [~,R(1:k,1:k)] = qrupdate(eye(k),R(1:k,1:k),R(1:k,k)-R(1:k,i), ...
+                                  ((1:k == i)-(1:k == k))');
+        R([i k],k+1:end) = R([k i],k+1:end);
+        r2([i k]) = r2([k i]);
+      end
+
+      % decrease rank from k to k-1
+      c2 = zeros(1,n-k+1);  % no R22 block
+      r2 = r2(1:k-1) - abs(R(1:k-1,1:k-1)\R(1:k-1,k)/R(k,k)).^2;
+      k = k - 1;
+      r = R(1:k,1:k)\R(1:k,k+1);
+      R(1:k,k+1:end) = [r R(1:k,k+2:end) + r*R(k+1,k+2:end)];
+      R = R(1:k,:);
+    end
+
+    if ~conv  % not converged
+      warning('FLAM:id:maxIterCount', ...
+              'Maximum number of RRQR iterations reached.')
+    end
+  end
+
+  % set ID outputs
+  sk = p(1:k); rd = p(k+1:end);
+  T = R(1:k,k+1:end);
 end
